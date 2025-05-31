b4f1a446ef07b5b1610d493a1525f517b70ccaad
mohammedaltayeb901 <shamoaltayeb@gmail.com>
2025-05-31 06:14:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,5 +6,4 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+app.run(host="0.0.0.0", port=3000)
